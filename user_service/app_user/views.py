9d361887987a6ed1755c629282c061976002ad77
Clint-Galendez-20210021497 <20210021497@my.xu.edu.ph>
2025-06-06 15:05:54 +0800

    feat: implement user registration view with token generation

--- user_service/app_user/views.py
+++ user_service/app_user/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserRegistrationSerializer
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(
+                {
+                    "user_info": {
+                        "id": user.id,
+                        "username": user.username,
+                        "email": user.email
+                    },
+                    "token": token.key
+                },
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
