caeab933422ee439e21c3fde5a425ae788d1bffd
Clint-Galendez-20210021497 <20210021497@my.xu.edu.ph>
2025-06-09 15:45:39 +0800

    feat: implement user login functionality with token generation

--- user_service/app_user/views.py
+++ user_service/app_user/views.py
@@ -1,11 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import UserRegistrationSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.contrib.auth import authenticate
+
+from .serializers import UserRegistrationSerializer
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UserRegistrationView(APIView):
@@ -26,3 +31,40 @@ class UserRegistrationView(APIView):
                 status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@method_decorator(csrf_exempt, name='dispatch')
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response(
+                {
+                    "user_info": {
+                        "id": user.id,
+                        "username": user.username,
+                        "email": user.email
+                    },
+                    "token": token.key
+                },
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            {"error": "Invalid Credentials"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            # Delete the token to log the user out
+            request.user.auth_token.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
