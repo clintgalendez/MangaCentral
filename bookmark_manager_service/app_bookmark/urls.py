5f0f1920e1843063069a3bc24cf131bde5f2d470
Clint-Galendez-20210021497 <20210021497@my.xu.edu.ph>
2025-06-12 10:03:24 +0800

    feat: add URL routing for bookmarks and supported sites

---
+++ bookmark_manager_service/app_bookmark/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('bookmarks/', views.BookmarkListCreateView.as_view(), name='bookmark-list-create'),
+    path('bookmarks/<uuid:pk>/', views.BookmarkDetailView.as_view(), name='bookmark-detail'),
+    path('bookmarks/<uuid:bookmark_id>/refresh/', views.refresh_bookmark, name='bookmark-refresh'),
+    path('supported-sites/', views.supported_sites, name='supported-sites'),
+]
